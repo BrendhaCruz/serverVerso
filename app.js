96f87ccda2a203559b9a32ee6fbb9eb7057e0454
Gustavo Oliveira <gustavo.hqo@gmail.com>
2017-11-28 20:06:11 -0300

    Configurando mongoose

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var db_config = require('./model/config/db_config');
+
+db_config();
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
---
+++ model/config/db_config.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+require('events').EventEmitter.prototype._maxListeners = 0;
+
+var db = mongoose.connection;
+
+module.exports = function() {
+    var perfil = 'versodb';
+
+    /**
+     * Caso não haja uma variável de ambiente informando o endereço do banco de dados,
+     * utilizar endereço local.
+     */
+    var db_string = process.env.MONGODB_ADDRESS || 'mongodb://127.0.0.1:27017/' + perfil;
+    if (!db.readyState) {
+        var conn = mongoose.connect(db_string);
+    }
+
+    db.once('error', console.error.bind(console, 'Something really strange happened...'));
+    db.once('open', function() {
+        db.opened = true;
+        console.log('DB Ready to go! ' + db_string);
+    });
+
+    return db;
+};
